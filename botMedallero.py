ae2a0661bfdcb395b2be6dfadc10ad0da3ddf376
fernand0 <fernand0@elmundoesimperfecto.com>
2024-07-27 20:04:11 +0200

    Adding mastodon

--- botMedallero.py
+++ botMedallero.py
@@ -44,12 +44,12 @@ def getData():
 
     medalsD = []
     try:
-        print(f"Url: {url}")
+        #print(f"Url: {url}")
         req = urllib.request.Request(url, data=None,
                                      headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36' })
         result = urllib.request.urlopen(req)
         res = result.read()
-        print(res)
+        #print(res)
         logging.debug(f"Result: {res}")
         soup = BeautifulSoup(res, "lxml")
         logging.debug(f"Soup: {soup}")
@@ -91,6 +91,7 @@ def printResults(msg, mode):
     else:
         dsts = {"twitter":"medalleroESP",
                 "telegram": "testFernand0",
+                "mastodon":"@medalleroESP@botsin.space",
                 }
 
     for dst in dsts:
